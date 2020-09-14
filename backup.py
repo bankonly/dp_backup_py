a23db793a0e6f1c40f40171f3689ad292410f47c
bankonly <for21bn01@gmail.com>
2020-09-14 14:35:35 +0700

    s

--- backup.py
+++ backup.py
@@ -1,30 +1,35 @@
 import os
 import platform
-from datetime import datetime
+import datetime
 from dotenv import load_dotenv
 from pexpect import popen_spawn
+import pytz
 
 # LOAD .ENV
 load_dotenv(dotenv_path="./.env")
-
-# target download folder name
-target_folder_download = datetime.today().strftime('%Y-%m-%d-%h')
 
 # Store key
 PROD_SERVER_PASSWORD = os.getenv("PROD_SERVER_PASSWORD")
 PROD_SERVER_IP = os.getenv("PROD_SERVER_IP")
 PROD_SERVER_USER = os.getenv("PROD_SERVER_USER")
+SAVE_PATH = os.getenv("SAVE_PATH")
 
-command = "sshpass -p '{password}' scp -r root@{server_ip}:/root/DB/{path} /d/db_backup".format(
-    server_ip=PROD_SERVER_IP, path=target_folder_download, password=PROD_SERVER_PASSWORD)
+# target download folder name
+target_folder_download = datetime.datetime.utcnow()
+target_folder_download = target_folder_download.replace(tzinfo=pytz.utc)
+target_folder_download = target_folder_download.strftime('%Y-%m-%d-%H')
+
+# default command
+command = "sshpass -p '{password}' scp -r root@{server_ip}:/root/DB/{path} {save_path}".format(
+    server_ip=PROD_SERVER_IP, path=target_folder_download, password=PROD_SERVER_PASSWORD, save_path=SAVE_PATH)
 
 # Detect OS
 detect_os = platform.system()
 
 # if detect_os is Windows then do this command
 if detect_os == "Windows":
-    command = "scp -r root@{server_ip}:/root/DB/{path} /d/db_backup".format(
-        server_ip=PROD_SERVER_IP, path=target_folder_download)
+    command = "scp -r root@{server_ip}:/root/DB/{path} {save_path}".format(
+        server_ip=PROD_SERVER_IP, path=target_folder_download, save_path=SAVE_PATH)
     runner = popen_spawn.PopenSpawn(cmd=command)
     runner.sendline(PROD_SERVER_PASSWORD)
 else:
